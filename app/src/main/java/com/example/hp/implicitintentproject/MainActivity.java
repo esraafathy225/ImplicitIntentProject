a105d8d5044ba5a5a3cf4463b6d13c0b1b9b6ac1
esraa.fathy225 <esraa.fathy225@gmail.com>
2018-10-03 23:37:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hp/implicitintentproject/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.hp.implicitintentproject;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button openMap,openBrowser,dial,takePicture,sendEmail,showContacts;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        openMap=findViewById(R.id.btn1);
+        openBrowser=findViewById(R.id.btn2);
+        dial=findViewById(R.id.btn3);
+        takePicture=findViewById(R.id.btn4);
+        sendEmail=findViewById(R.id.btn5);
+        showContacts=findViewById(R.id.btn6);
+
+        openMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri=Uri.parse("geo:45.455,-46.68");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+        openBrowser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri=Uri.parse("http://www.google.com");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+        dial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri=Uri.parse("tel:124467751");
+                Intent intent=new Intent(Intent.ACTION_DIAL,uri);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+        takePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+        sendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_SEND);
+                intent.setType("*/*");
+                intent.putExtra(Intent.EXTRA_TEXT,"write your email");
+                //forcing an app chooser
+                Intent chooser=Intent.createChooser(intent,"Choose an app");
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(chooser);
+                }
+            }
+        });
+        showContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri=Uri.parse("content://contacts/people/");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
