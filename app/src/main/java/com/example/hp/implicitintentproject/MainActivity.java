fb19fd1193471d24fd3e6c52bd7d25df854f648b
esraa.fathy225 <esraa.fathy225@gmail.com>
2023-03-05 19:36:54 +0200

    Edit

--- app/src/main/java/com/example/hp/implicitintentproject/MainActivity.java
+++ app/src/main/java/com/example/hp/implicitintentproject/MainActivity.java
@@ -2,8 +2,9 @@ package com.example.hp.implicitintentproject;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.provider.ContactsContract;
 import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -77,8 +78,9 @@ public class MainActivity extends AppCompatActivity {
         showContacts.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Uri uri=Uri.parse("content://contacts/people/");
-                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
                 if (intent.resolveActivity(getPackageManager()) != null) {
                     startActivity(intent);
                 }
